ea9049d05d69eb39d05592220bcde4e3c0a69eb4
Andrii <atsapko@supportyourapp.com>
2020-08-14 01:00:14 +0300

    Updates

---
+++ precache-manifest.c6cc890dc4404f1bc8b878a93bdd1660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6474e8f69f25849de4fbfd350681274",
+    "url": "/dagbladet/index.html"
+  },
+  {
+    "revision": "41dd079254bee143e80b",
+    "url": "/dagbladet/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "298d6793b41c1ecb4b6b",
+    "url": "/dagbladet/static/css/main.ff550775.chunk.css"
+  },
+  {
+    "revision": "41dd079254bee143e80b",
+    "url": "/dagbladet/static/js/2.42ca6112.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dagbladet/static/js/2.42ca6112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298d6793b41c1ecb4b6b",
+    "url": "/dagbladet/static/js/main.eb44c8bc.chunk.js"
+  },
+  {
+    "revision": "d3bdfd0575b129acc175",
+    "url": "/dagbladet/static/js/runtime-main.a478dd27.js"
+  }
+]);
